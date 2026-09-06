ddbd4efcbda6d74f04a990624630d319c9dc3775
hocn883 <hocn883@gmail.com>
2026-09-07 02:43:55 +0700

    update api

--- Web_Frontend/myapp/src/utils/api.js
+++ Web_Frontend/myapp/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="https://interhub1611.onrender.com";
+const BASE_URL="https://interhub1611.onrender.com/api";
 
 const api=axios.create({
   baseURL:BASE_URL,
