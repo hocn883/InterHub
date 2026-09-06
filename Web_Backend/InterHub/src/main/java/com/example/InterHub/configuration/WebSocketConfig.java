42878c3dacdc6c9ce3dff9bd3624590551f20fd3
hocn883 <hocn883@gmail.com>
2026-09-07 04:15:20 +0700

    update

--- Web_Backend/InterHub/src/main/java/com/example/InterHub/configuration/WebSocketConfig.java
+++ Web_Backend/InterHub/src/main/java/com/example/InterHub/configuration/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.InterHub.config;
+package com.example.InterHub.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
