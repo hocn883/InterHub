6be30df109a3ad6044d8bfcc0b05908cf4118e87
hocn883 <hocn883@gmail.com>
2026-09-16 14:35:50 +0700

    final

--- Web_Backend/InterHub/src/main/java/com/example/InterHub/dto/request/PostJobRequest.java
+++ Web_Backend/InterHub/src/main/java/com/example/InterHub/dto/request/PostJobRequest.java
@@ -7,28 +7,38 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 @Setter
 @Getter
 public class PostJobRequest {
+
         @NotBlank
         private String title;
+
         @NotBlank
         private String description;
+
         private String requirements;
+
         @NotNull
         private BigDecimal salary;
+
         @NotNull
         private Integer quantity;
-        @NotNull
+
         private Double longitude;
-        @NotNull
+
         private Double latitude;
+
         @NotBlank
         private String location;
+
         @NotNull
         private LocalDate deadline;
+
         @NotNull
         private LocalDate startDate;
+
         @NotNull
         private LocalDate endDate;
-    }
+}
